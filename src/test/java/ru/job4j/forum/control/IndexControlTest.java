972aa1445ab59268a794f9ea7a7c7d6944ec9236
saimon494 <saimon.494@gmail.com>
2021-08-01 14:26:52 +0300

    0. Spring test [#6881], исправление

--- src/test/java/ru/job4j/forum/control/IndexControlTest.java
+++ src/test/java/ru/job4j/forum/control/IndexControlTest.java
@@ -5,21 +5,30 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.jupiter.api.Test;
 
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.forum.Main;
+import ru.job4j.forum.service.PostService;
+
+import java.util.List;
 
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
 public class IndexControlTest {
+
+    @MockBean
+    private PostService postService;
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     public void whenGetIndexPage() throws Exception {
+        Mockito.doReturn(List.of()).when(postService).findAll();
         this.mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("index"))
--- src/test/java/ru/job4j/forum/control/PostControlTest.java
+++ src/test/java/ru/job4j/forum/control/PostControlTest.java
@@ -4,19 +4,27 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 import org.junit.jupiter.api.Test;
 
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.forum.Main;
+import ru.job4j.forum.model.Post;
+import ru.job4j.forum.service.PostService;
 
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
 public class PostControlTest {
+
+    @MockBean
+    private PostService postService;
 
     @Autowired
     private MockMvc mockMvc;
@@ -24,7 +32,9 @@ public class PostControlTest {
     @Test
     @WithMockUser
     public void whenGetPostPage() throws Exception {
-        this.mockMvc.perform(get("/post").param("postId", "1"))
+        Mockito.doReturn(new Post()).when(postService).findById(1);
+        this.mockMvc.perform(get("/post?postId=1"))
+                .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("post"))
                 .andExpect(model().attributeExists("post"));
