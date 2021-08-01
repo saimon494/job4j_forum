0501b148a528ac50a5478eecb043655b386b7bcf
saimon494 <saimon.494@gmail.com>
2021-08-02 00:25:29 +0300

    0. Spring test [#6881], исправление 2

--- src/test/java/ru/job4j/forum/control/IndexControlTest.java
+++ src/test/java/ru/job4j/forum/control/IndexControlTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.forum.control;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.Test;
@@ -10,9 +11,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.forum.Main;
+import ru.job4j.forum.model.User;
 import ru.job4j.forum.service.PostService;
+import ru.job4j.forum.service.UserService;
 
 import java.util.List;
 
@@ -23,15 +27,22 @@ public class IndexControlTest {
     @MockBean
     private PostService postService;
 
+    @MockBean
+    private UserService userService;
+
     @Autowired
     private MockMvc mockMvc;
 
     @Test
+    @WithMockUser(username = "user")
     public void whenGetIndexPage() throws Exception {
         Mockito.doReturn(List.of()).when(postService).findAll();
-        this.mockMvc.perform(get("/"))
+        Mockito.doReturn(new User()).when(userService).findByUsername("user");
+        this.mockMvc.perform(get("/index"))
+                .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("index"))
-                .andExpect(model().attributeExists("posts"));
+                .andExpect(model().attributeExists("posts"))
+                .andExpect(model().attributeExists("user"));
     }
 }
